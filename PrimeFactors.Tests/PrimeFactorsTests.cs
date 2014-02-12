31ecf34ae2c477afc67a053df3ddbebe468d1700
Kevin Poston <kevposton@gmail.com>
2014-02-12 08:53:04 -0500

    Passed the first test

---
+++ PrimeFactors.Tests/PrimeFactorsTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+
+namespace PrimeFactors.Tests
+{
+    [TestFixture]
+    public class PrimeFactorsTests
+    {
+        [Test]
+        public void TestGenerateForOne()
+        {
+            Assert.That(new List<Int32>(), Is.EqualTo(PrimeFactors.Generate(1)));
+        }
+    }
+}
---
+++ PrimeFactors/PrimeFactors.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrimeFactors
+{
+    public class PrimeFactors
+    {
+        public static List<Int32> Generate(Int32 number)
+        {
+            return new List<Int32>();
+        }
+
+    }
+}
