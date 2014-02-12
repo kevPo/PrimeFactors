9db2e4490fed6fe87a03ef5fc7bf2e9c83e1a3da
Kevin Poston <kevposton@gmail.com>
2014-02-12 09:02:59 -0500

    Passed test for three

--- PrimeFactors.Tests/PrimeFactorsTests.cs
+++ PrimeFactors.Tests/PrimeFactorsTests.cs
@@ -20,5 +20,10 @@ namespace PrimeFactors.Tests
             Assert.That(new List<Int32>{2}, Is.EqualTo(PrimeFactors.Generate(2)));
         }
 
+        [Test]
+        public void TestGenerateForThree()
+        {
+            Assert.That(new List<Int32> { 3 }, Is.EqualTo(PrimeFactors.Generate(3)));
+        }
     }
 }
